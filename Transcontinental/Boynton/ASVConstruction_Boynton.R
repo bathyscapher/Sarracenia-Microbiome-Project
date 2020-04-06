ee44144ae639f761ad298bf775df59a43e8440c9
bathyscapher <korn@cumulonimbus.at>
2020-04-06 18:55:52 +0200

    simplified header in scripts, added cutadapt workflow

--- Transcontinental/Boynton/ASVConstruction_Boynton.R
+++ Transcontinental/Boynton/ASVConstruction_Boynton.R
@@ -1,9 +1,6 @@
 ################################################################################
 ################################################################################
-################################################################################
-################################################################################
-### SMP ASV Bittleson data
-### Author: korn@cumulonimbus.at University of Fribourg 2020
+### SMP ASV Boynton data
 ################################################################################
 
 rm(list = ls())
@@ -11,6 +8,8 @@ rm(list = ls())
 
 # library("rSFFreader")
 library("dada2")
+library("ShortRead")
+library("Biostrings")
 library("DECIPHER")
 library("gridExtra")
 
@@ -52,6 +51,83 @@ rF <- sort(list.files(pattern = "gz", full.names = TRUE))
 sample.names <- sapply(strsplit(basename(rF), "\\."), `[`, 1)
 
 
+### Check for primers
+## Remove Ns from reads
+rF.fN <- file.path("filtN", basename(rF))
+filterAndTrim(rF, rF.fN, maxN = 0, multithread = TRUE)
+
+
+## Forward and reverse primer
+# FWD <- "CTGCGGAAGGATCATTACAGT" # C. pseudoglaebosa
+# REV <- "TGTTCAGACAACACTGTTCA"
+# FWD <- "AAGTCGTAACAAGGTTTCCG" # R. babjevae
+# REV <- "CCCAACTCGGCTCTAGTAA"
+# FWD <- "GGTAATGCGGTCGTCTAAAA" # M. aphidis
+# REV <- "CTCTTCCAAAGAAGCGAGG"
+
+FWD <- "CTTGGTCATTTAGAGGAAGTAA" # ITS1F
+REV <- "TCCTCCGCTTATTGATATGC" # ITS4
+# FWD <- "CCATCTCATCCCTGCGTGTCTCCGACTCAG" # 454 "A" primer
+# REV <- "CCTATCCCCTGTGTGCCTTGGCAGTCTCAG" # 454 "B" primer
+
+## Compile all orientations of the primers
+allOrients <- function(primer) {
+  dna <- DNAString(primer)
+  orients <- c(Forward = dna, Complement = complement(dna),
+               Reverse = reverse(dna), RevComp = reverseComplement(dna))
+  return(sapply(orients, toString))  # Convert back to character vector
+}
+
+FWD.orients <- allOrients(FWD)
+REV.orients <- allOrients(REV)
+
+
+## Count occurence of all primer orientations in the reads
+primerHits <- function(primer, fn) {
+  nhits <- vcountPattern(primer, sread(readFastq(fn)), fixed = FALSE)
+  return(sum(nhits > 0))
+}
+
+(reads <- ceiling(runif(1, 1, length(rF.fN))))
+rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = rF.fN[[reads]]),
+      REV.ForwardReads = sapply(REV.orients, primerHits, fn = rF.fN[[reads]]))
+
+
+## Cut the primers
+cutadapt <- "/usr/bin/cutadapt"
+# system2(cutadapt, args = "--version")
+
+
+path.cut <- file.path(".", "cutPrimers")
+if(!dir.exists(path.cut)) dir.create(path.cut)
+rF.cut <- file.path(path.cut, basename(rF.fN))
+
+
+## Trim FWD and the reverse-complement of REV off of R1 &  REV and the
+## reverse-complement of FWD off of R2
+FWD.RC <- dada2:::rc(FWD)
+REV.RC <- dada2:::rc(REV)
+
+R1.flags <- paste("-g", FWD, "-a", REV.RC)
+R2.flags <- paste("-g", REV, "-a", FWD.RC)
+# R2.flags <- paste("-G", REV, "-A", FWD.RC)
+
+# Run Cutadapt
+for(i in seq_along(rF)) {
+  system2(cutadapt, args = c(R1.flags, R2.flags, "-n", 2,
+                             "-o", rF.cut[i], # output
+                             rF.fN[i])) # input
+}
+
+
+(reads <- ceiling(runif(1, 1, length(rF.cut))))
+rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = rF.cut[[reads]]),
+      REV.ForwardReads = sapply(REV.orients, primerHits, fn = rF.cut[[reads]]))
+
+rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = rF.fN[[reads]]),
+      REV.ForwardReads = sapply(REV.orients, primerHits, fn = rF.fN[[reads]]))
+
+
 ### Filter and trim. Place filtered files in filtered/ subdirectory
 rF.f <- file.path("filtered", paste0(sample.names, "_filt.fastq.gz"))
 
@@ -59,9 +135,11 @@ names(rF.f) <- sample.names
 
 
 ### Filtering, min and max values as in Boynton et al. 2019
-filt <- filterAndTrim(rF, rF.f, minLen = 200, maxLen = 1000,
-                      maxN = 0, rm.phix = TRUE, truncQ = 2, #maxEE = c(2, 2),
+filt <- filterAndTrim(rF.cut, rF.cut.f, minLen = 200, maxLen = 1000,
+                      maxN = 0, rm.phix = TRUE, truncQ = 2,
                       compress = TRUE, multithread = ncore, verbose = TRUE)
+
+names(rF.f) <- sample.names
 
 
 ### Read quality profiles exemplarily
@@ -80,21 +158,17 @@ errF <- learnErrors(rF.f, multithread = ncore, verbose = TRUE)
 plotErrors(errF, nominalQ = TRUE)
 
 
+## Dereplicate identical reads
+derep.rF <- derepFastq(rF.f, verbose = TRUE)
+names(derep.rF) <- sample.names
+
+
 ### Core sample inference algorithm
-dadaF <- dada(rF.f, errF, HOMOPOLYMER_GAP_PENALTY = -1, BAND_SIZE = 32,
+dadaF <- dada(derep.rF, errF, HOMOPOLYMER_GAP_PENALTY = -1, BAND_SIZE = 32,
               multithread = ncore)
 # dadaFs[[1]]
 
-
 # saveRDS(dadaF, "dadaF_PB.rds")
-
-
-### Merge paired reads
-# contigs <- mergePairs(dadaF, rF.f, dadaR, rR.f, verbose = TRUE)
-# head(contigs[[1]])
-
-
-# saveRDS(contigs, "contigs_PB.rds")
 
 
 ### Construct ASV table
@@ -123,11 +197,9 @@ getN <- function(x){
   sum(getUniques(x))
   }
 
-track <- cbind(out, sapply(dadaF, getN), sapply(dadaR, getN),
-               sapply(contigs, getN), rowSums(seqtab.nochim))
+track <- cbind(filt, sapply(dadaF, getN), rowSums(seqtab.nochim))
 
-colnames(track) <- c("input", "filtered", "denoisedF", "denoisedR", "merged",
-                     "nonchim")
+colnames(track) <- c("input", "filtered", "denoisedF", "nonchim")
 rownames(track) <- sample.names
 head(track)
 
@@ -135,7 +207,7 @@ head(track)
 ### Assign taxonomy with IdTaxa and SILVA
 dna <- DNAStringSet(getSequences(seqtab.nochim))
 
-load("~/Desktop/SMP_unsynced/silva/SILVA_SSU_r138_2019.RData")
+load("~/Desktop/SMP_unsynced/silva/UNITE_v2020_February2020.RData")
 
 
 ids <- IdTaxa(dna, trainingSet, strand = "top", processors = ncore,
@@ -157,7 +229,5 @@ rownames(taxa.id) <- getSequences(seqtab.nochim)
 saveRDS(taxa.id, "taxa.id_PB.rds")
 
 
-################################################################################
-################################################################################
 ################################################################################
 ################################################################################
