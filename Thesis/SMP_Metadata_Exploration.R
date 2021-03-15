3bdff4dedcce59f8a53f62f2e3800c5616bba08a
Bathyscapher <korn@cumulonimbus.at>
2021-03-15 21:48:43 +0100

    Updated

--- Thesis/SMP_Metadata_Exploration.R
+++ Thesis/SMP_Metadata_Exploration.R
@@ -13,19 +13,15 @@ library("ggplot2")
 library("GGally")
 theme_set(theme_bw(base_size = 20) +
             theme(rect = element_rect(fill = "transparent")))
-library("vegan")
-library("ape")
-library("gplots")
+# library("vegan")
 library("reshape2")
-# library("scales")
-# library("cluster")
-# library("apcluster")
 
 
-# setwd("~/Sarracenia-Microbiome-Project/Thesis")
-setwd("~/Seafile/_FNS_2016-NicheDynamCoexist/SarraceniaMicrobiomeProject")
+rm(list = ls())
+setwd("~/Sarracenia-Microbiome-Project/Thesis")
 
-# rm(list = ls())
+
+set.seed(34706)
 
 
 ################################################################################
@@ -35,27 +31,102 @@ gradCol <- c("#D55E00", "#E69F00", "#F0E442", "#009E73", "#56B4E9")
 
 ################################################################################
 ### Pitcher data
-tst.prok.a <- readRDS("SMP_prok.a.RDS")
-tst.euk.a <- readRDS("SMP_euk.a.RDS")
-tst.smp <- readRDS("SMP_smp.RDS")
+prok.a <- readRDS("SMP_prok.a.RDS")
+euk.a <- readRDS("SMP_euk.a.RDS")
 
 
 ################################################################################
 ### Moss taxa
 ## Merged
-# moss.prok.a <- readRDS("SMP_moss.prok.a.RDS")
-# moss.euk.a <- readRDS("SMP_moss.euk.a.RDS")
-# moss <- readRDS("SMP_moss.RDS")
-
-
-## Separately
-moss.prok.a <- readRDS("Mosses/SMP_moss40.prok.a.RDS")
-moss.euk.a <- readRDS("Mosses/SMP_moss40.euk.a.RDS")
-moss <- readRDS("Mosses/SMP_moss40.RDS")
+moss.prok.a <- readRDS("SMP_moss20.prok.a.RDS")
+moss.euk.a <- readRDS("SMP_moss20.euk.a.RDS")
+# moss <- readRDS("SMP_moss20.RDS")
 
 
 ################################################################################
-### Export dataframe with taxa and abundances
+### nMDS pitchers
+## Choose pro- or eukaryotes
+# primer <- "16S"
+primer <- "18S"
+
+
+if(primer == "16S")
+  {smp <- prok.a}
+if(primer == "18S")
+  {smp <- euk.a}
+
+
+## log-transform
+smp.log <- transform_sample_counts(smp, function(otu) {log1p(otu)})
+
+
+## nMDS
+smp.nmds <- ordinate(smp.log, method = "NMDS", distance = "bray", k = 4,
+                     autotransform = FALSE, trymax = 100)
+smp.nmds
+stressplot(smp.nmds)
+
+
+## Plot nMDS
+plot_ordination(smp, smp.nmds, shape = "Succession", color = "Site",
+                title = NULL, axes = 1:2) +
+  stat_ellipse(aes(group = Site), type = "t", linetype = 2, size = 0.2) +
+  geom_point(size = 3) +
+  scale_colour_manual(values = gradCol) +
+  coord_fixed(ratio = 1) +
+  theme(legend.position = "top", legend.direction = "horizontal",
+        legend.box = "vertical") +
+  xlab("nMDS1") +
+  ylab("nMDS2")
+# ggsave("SMP_16S_nMDS_ASV.pdf", width = 8.27, height = 8.27)
+# ggsave("SMP_18S_nMDS_ASV.pdf", width = 8.27, height = 8.27)
+
+
+### nMDS pitchers and mosses
+## Choose pro- or eukaryotes
+# primer <- "16S"
+primer <- "18S"
+
+if(primer == "16S")
+  {smp.moss <- merge_phyloseq(prok.a, moss.prok.a)}
+if(primer == "18S")
+  {smp.moss <- merge_phyloseq(euk.a, moss.euk.a)}
+
+
+## log-transform
+smp.moss.log <- transform_sample_counts(smp.moss, function(otu) {log1p(otu)})
+
+
+## nMDS
+smp.moss.nmds <- ordinate(smp.moss.log, method = "NMDS", distance = "bray",
+                          k = 4, autotransform = FALSE, trymax = 100,
+                          weakties = FALSE)
+smp.moss.nmds
+stressplot(smp.moss.nmds)
+
+
+## Plot nMDS
+plot_ordination(smp.moss, smp.moss.nmds, shape = "Site", color = "Succession",
+                title = NULL, axes = 1:2) +
+  stat_ellipse(aes(group = Succession), type = "t", linetype = 2, size = 0.2) +
+  geom_point(size = 3) +
+  scale_colour_manual(values = gradCol[c(2, 1, 4)]) +
+  coord_fixed(ratio = 1) +
+  theme(legend.position = "top", legend.direction = "horizontal",
+        legend.box = "vertical") +
+  xlab("nMDS1") +
+  ylab("nMDS2")
+# ggsave("SMP_16S_nMDS_withMoss.pdf", width = 8.27, height = 8.27)
+# ggsave("SMP_18S_nMDS_withMoss.pdf", width = 8.27, height = 8.27)
+
+
+################################################################################
+### Plot taxa
+## Load pro- and eukaryotic taxa together
+smp <- readRDS("SMP_smp.RDS")
+
+
+### Dataframe with taxa and abundances
 ## Pitchers
 tax.l <- as.data.frame(smp@tax_table@.Data)
 otu.l <- t(as.data.frame(otu_table(smp)))
@@ -66,224 +137,8 @@ colnames(tax.otu.l)[1] <- "Taxa"
 
 rm(tax.l, otu.l)
 
-# write.table(tax.otu.l, "SMP_OTU_Leaf.csv", sep = "\t", row.names = FALSE)
-# write.table(tax.otu.l, "SMP_ASV_Leaf.csv", sep = "\t", row.names = FALSE)
 
-
-## Mosses
-tax.m <- as.data.frame(moss@tax_table@.Data)
-otu.m <- as.data.frame(t(otu_table(moss)))
-
-tax.otu.m <- merge(tax.m, otu.m, by = 0, all = TRUE)
-rownames(tax.otu.m) <- tax.otu.m$Row.names
-tax.otu.m$Row.names <- NULL
-
-rm(tax.m, otu.m)
-
-
-# write.table(tax.otu.m, "SMP_OTU_16S_Moss.csv", sep = "\t", row.names = FALSE)
-# write.table(tax.otu.m, "SMP_OTU_18S_Moss.csv", sep = "\t", row.names = FALSE)
-
-
-################################################################################
-### nMDS pitchers
-smp.log <- transform_sample_counts(euk.a, function(otu) {log1p(otu)})
-
-
-## Eukaryotes only: remove outlier with only one Saccharomyces (LM3a08L)
-smp.log <- prune_samples(smp.log@sam_data$FullID != "LM3a08L", smp.log)
-any(taxa_sums(smp.log) == 0)
-
-smp.nmds <- ordinate(smp.log, method = "NMDS", distance = "bray", k = 4,
-                     autotransform = FALSE, trymax = 100)
-# smp.nmds <- ordinate(smp.log, method = "PCoA", distance = "bray")
-smp.nmds
-
-
-stressplot(smp.nmds)
-# barplot(smp.nmds$values$Relative_eig)
-# biplot(smp.nmds, data.frame(otu_table(smp.a)))
-
-
-plot_ordination(smp, smp.nmds, shape = "Succession",
-                color = "Site", title = NULL, #label = "FullID",
-                axes = 1:2) +
-  stat_ellipse(aes(group = Site), type = "t", linetype = 2, size = 0.2) +
-  geom_point(size = 3) +
-  scale_colour_manual(values = gradCol) +
-  coord_fixed(ratio = 1) +
-  theme(legend.position = "top", legend.direction = "horizontal",
-        legend.box="vertical") +
-  # guides(fill = guide_legend(nrow = 2, byrow = TRUE)) + # grrr
-  xlab("nMDS1") +
-  ylab("nMDS2")
-# ggsave("SMP_16S_nMDS_ASV.pdf", width = 8.27, height = 8.27)
-# ggsave("SMP_18S_nMDS_ASV.pdf", width = 8.27, height = 8.27)
-# ggsave("SMP_nMDS_ASV.pdf", width = 8.27, height = 8.27)
-
-
-### nMDS pitchers and mosses
-smp.moss <- merge_phyloseq(prok.a, moss.prok.a)
-smp.moss <- merge_phyloseq(euk.a, moss.euk.a)
-
-
-smp.moss.log <- transform_sample_counts(smp.moss, function(otu) {log1p(otu)})
-
-
-smp.moss.nmds <- ordinate(smp.moss.log, method = "NMDS", distance = "bray",
-                          k = 4, autotransform = FALSE, trymax = 100,
-                          weakties = FALSE)
-smp.moss.nmds
-
-
-stressplot(smp.moss.nmds)
-# barplot(smp.moss.nmds$values$Relative_eig)
-# biplot(smp.moss.nmds, data.frame(otu_table(smp.a)))
-
-
-plot_ordination(smp.moss, smp.moss.nmds, shape = "Succession",
-                color = "Site", title = NULL, #label = "FullID",
-                axes = 1:2) +
-  stat_ellipse(aes(group = Succession), type = "t", linetype = 2, size = 0.2) +
-  geom_point(size = 3) +
-  scale_colour_manual(values = gradCol) +
-  coord_fixed(ratio = 1) +
-  theme(legend.position = "top", legend.direction = "horizontal") +
-  guides(linetype = guide_legend(nrow = 2)) +
-  xlab("nMDS1") +
-  ylab("nMDS2")
-# ggsave("SMP_16S_nMDS_withMoss.pdf", width = 8.27, height = 8.27)
-# ggsave("SMP_18S_nMDS_withMoss.pdf", width = 8.27, height = 8.27)
-
-
-################################################################################
-### PERMANOVA
-## Prokaryotes
-tax.l <- as.data.frame(prok.a@tax_table@.Data)
-otu.l <- t(as.data.frame(otu_table(prok.a)))
-
-tax.otu.prok <- merge(tax.l, otu.l, by = 0, all = TRUE)
-rownames(tax.otu.prok) <- tax.otu.prok$Row.names
-colnames(tax.otu.prok)[1] <- "Taxa"
-
-tax.otu.prok.t <- data.frame(t(tax.otu.prok[c(8:167)]))
-tax.otu.prok.t$Site <- gsub(".{5}$", "", rownames(tax.otu.prok.t))
-
-rm(tax.l, otu.l, tax.otu.prok)
-
-
-prok.adonis <- adonis(tax.otu.prok.t[, -346] ~ tax.otu.prok.t$Site,
-                      method = "bray", perm = 999)
-summary(prok.adonis)
-prok.adonis
-
-
-## Eukaryotes
-tax.l <- as.data.frame(euk.a@tax_table@.Data)
-otu.l <- t(as.data.frame(otu_table(euk.a)))
-
-tax.otu.euk <- merge(tax.l, otu.l, by = 0, all = TRUE)
-rownames(tax.otu.euk) <- tax.otu.euk$Row.names
-colnames(tax.otu.euk)[1] <- "Taxa"
-
-tax.otu.euk.t <- data.frame(t(tax.otu.euk[c(8:166)]))
-tax.otu.euk.t$Site <- gsub(".{5}$", "", rownames(tax.otu.euk.t))
-
-rm(tax.l, otu.l, tax.otu.euk)
-
-
-euk.adonis <- adonis(tax.otu.euk.t[, -115] ~ tax.otu.euk.t$Site, method = "bray",
-                     perm = 999)
-euk.adonis
-
-
-## Combined
-tax.otu.t <- data.frame(t(tax.otu.l[c(8:167)]))
-tax.otu.t$Site <- gsub(".{5}$", "", rownames(tax.otu.t))
-
-
-smp.adonis <- adonis(tax.otu.t[, -460] ~ tax.otu.t$Site, method = "bray",
-                     perm = 999)
-smp.adonis
-
-
-################################################################################
-### Clustering
-## UPGMA
-smp.bc <- vegdist(t(tax.otu.prok[, -c(1:7)]), method = "bray")
-
-smp.bc.UPGMA <- hclust(smp.bc, method = "average")
-plot(smp.bc.UPGMA, main = "", cex = 0.5)
-
-
-smp.bc.UPGMA.coph <- cophenetic(smp.bc.UPGMA)
-cor(smp.bc, smp.bc.UPGMA.coph)
-
-
-plot(smp.bc, smp.bc.UPGMA.coph, xlab = "Bray-Curtis dissimilarity",
-     ylab="Cophenetic distance", asp=1, xlim=c(0,1), ylim=c(0,1),
-     main=c(paste("UPGMA \n", "Cophenetic correlation =",
-                  round(cor(smp.bc, smp.bc.UPGMA.coph),3))),
-     cex=0.8, cex.main=0.9)
-abline(0,1)
-lines(lowess(smp.bc, smp.bc.UPGMA.coph), col="red")
-
-
-asw <- numeric(nrow(t(tax.otu.prok[, -c(1:7)])))
-
-for (k in 2:(nrow(t(tax.otu.prok[, -c(1:7)])) - 1)) {
-  sil <- silhouette(cutree(smp.bc.UPGMA, k = k), smp.bc)
-  asw[k] <- summary(sil)$avg.width
-}
-k.best <- which.max(asw)
-
-plot(1:nrow(t(tax.otu.prok[, -c(1:7)])), asw, type = "h",
-     xlab = "k", ylab = "Average silhouette width")
-axis(1, k.best, paste(k.best, sep = "\n"), col = "red", font = 2,
-     col.axis = "red")
-points(k.best, max(asw), pch = 16, col = "red", cex = 1.5)
-cat("", "Silhouette-optimal number of clusters k =", k.best, "\n",
-    "with an average silhouette width of", max(asw), "\n")
-
-
-## AP Clustering
-d.apclus <- apcluster(negDistMat(r = 1), as.matrix(smp.bc))
-
-cat("affinity propogation optimal number of clusters:",
-    length(d.apclus@clusters), "\n")
-
-
-# pdf("SMP_APCluster_prok.pdf", height = 8.27, width = 8.27)
-# pdf("SMP_APCluster_euk.pdf", height = 8.27, width = 8.27)
-# pdf("SMP_APCluster_all.pdf", height = 8.27, width = 8.27)
-heatmap(d.apclus, as.matrix(smp.bc), margin = c(3.5, 3.5),
-        cexRow = 0.3, cexCol = 0.3)
-dev.off()
-
-
-plot(d.apclus, as.matrix(smp.bc))
-
-
-################################################################################
-### Plot taxa
-smp.log <- transform_sample_counts(smp, function(otu) {log1p(otu)})
-
-plot_bar(smp.log, x = "Class", fill = "Site") +
-  geom_bar(aes(color = Site, fill = Site), stat = "identity",
-           position = "stack") +
-  facet_grid( ~ Domain, scales = "free", space = "free_y") +
-  theme(legend.position = "top") +
-  scale_color_manual(values = gradCol) +
-  scale_fill_manual(values = gradCol) +
-  xlab("") +
-  ylab("ln+1(Abundance) [%]") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  coord_flip()
-# ggsave("SMP_Phyla_prok.pdf", width = 8.27, height = 11.69)
-# ggsave("SMP_Phyla_euk.pdf", width = 8.27, height = 11.69)
-
-
-### Without S4-class
+### log-transform abundances
 tax.otu.l[, -c(1:7)] <- log1p(tax.otu.l[, -c(1:7)])
 
 
@@ -300,7 +155,7 @@ levels(tax.otu.lm$Succession) <- c("Early", "Late")
 tax.otu.lm$Domain[tax.otu.lm$Domain == "Archaea"] <- "A."
 
 
-## Reorder taxonomically
+## Reorder by taxonomy
 tax.otu.lm[, c(2:7)] <- lapply(tax.otu.lm[, c(2:7)], as.factor)
 
 
@@ -334,7 +189,7 @@ tax <- c("Crenarchaeota", ## Archaea
 
 
 ## Pronounce small values even more
-tax.otu.lm$Count[tax.otu.lm$Count < 0.01 & tax.otu.lm$Count > 0] <- 0.1
+tax.otu.lm$Count[tax.otu.lm$Count < 0.1 & tax.otu.lm$Count > 0] <- 0.1
 
 summary(tax.otu.lm$Count)
 
@@ -354,7 +209,7 @@ ggplot(tax.otu.lm, aes(x = factor(Phylum, level = rev(tax)), y = Count,
 # ggsave("SMP_PhylaOverview.pdf", width = 8.27, height = 11.69)
 
 
-aggregate(Count ~ Class, data = tax.otu.lm, FUN = sum)
+# aggregate(Count ~ Class, data = tax.otu.lm, FUN = sum)
 
 
 ### Inspect taxa and frequencies
@@ -383,12 +238,11 @@ inc$Incidence <- rowSums(inc[, -c(1:2)])
 
 ## Sort
 inc <- inc[order(inc$Incidence), ]
-inc$Phylum <- factor(inc$Phylum,
-                                    levels = unique(as.character(
-                                      inc$Phylum)))
+inc$Phylum <- factor(inc$Phylum, levels = unique(as.character(inc$Phylum)))
 inc$IncidencePercent <- 100 / 160 * inc$Incidence
 
 
+## Abbreviate archaea
 inc$Domain[inc$Domain == "Archaea"] <- "A."
 
 
@@ -401,49 +255,7 @@ ggplot(inc, aes(x = Phylum, y = IncidencePercent)) +
   xlab("") +
   coord_flip() +
   theme_bw(base_size = 10)
-ggsave("SMP_Incidence.pdf", width = 8.27, height = 11.69 / 2)
-
-
-
-## Control
-# tax.log <- as.data.frame(smp.log@tax_table@.Data)
-# otu.log <- t(as.data.frame(otu_table(smp.log)))
-#
-# tax.otu.log <- merge(tax.log, otu.log, by = 0, all = TRUE)
-# rownames(tax.otu.log) <- tax.otu.l$Row.names
-# colnames(tax.otu.log)[1] <- "Taxa"
-#
-# tax.otu.logm <- reshape2::melt(tax.otu.log, id.vars = c("Taxa", "Domain",
-#                                                         "Phylum", "Class",
-#                                                         "Order", "Family",
-#                                                         "Genus"),
-#                                variable.name = "FullID", value.name = "Count")
-#
-# aggregate(Count ~ Class, data = tax.otu.logm, FUN = sum)
-
-
-### Check for (spurious) taxa
-rank_names(smp)
-smp.taxa <- get_taxa_unique(smp, taxonomic.rank = rank_names(smp))
-# moss.taxa <- get_taxa_unique(moss.a, taxonomic.rank = rank_names(moss.a)[5])
-
-taxa.g <- grep("unknown", smp.taxa, ignore.case = TRUE)
-smp.taxa[taxa.g]
-
-
-## Explore taxa
-smp.sub <- subset_taxa(smp, (Phylum %in% c("Rotifera")))
-tax.df <- data.frame(tax_table(smp.sub))
-
-summary(is.na(tax.df$Genus))
-dim(tax.df)
-
-otu.df <- data.frame(otu_table(smp.sub))
-otu.df[otu.df > 1] <- 1
-rowSums(otu.df)
-summary(rowSums(otu.df) == 0)
-
-get_taxa_unique(smp.sub, taxonomic.rank = "Genus")
+# ggsave("SMP_Incidence.pdf", width = 8.27, height = 11.69 / 2)
 
 
 ################################################################################
